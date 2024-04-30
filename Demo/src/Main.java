068138c29dd26115ddfdb1b67487454bf26c0e70
NQuynhAnh <nguyenquynh010920@gmail.com>
2024-04-30 07:15:43 +0700

    Chương trình Java đầu tiên

--- Demo/src/Main.java
+++ Demo/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Xin chào lớp Java Bacsic 01-2024");
+        System.out.println("Rất vui được gặp bạn");
     }
 }
