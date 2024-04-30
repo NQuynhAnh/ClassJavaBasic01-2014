bf56b7b56086c50a89daa666d179d8baca631b6b
NQuynhAnh <nguyenquynh010920@gmail.com>
2024-04-30 07:37:56 +0700

    ...

---
+++ Demo/src/Buoi1/Buoi1.java
@@ -0,0 +1,10 @@
+package Buoi1;
+
+public class Buoi1 {
+    public static void main (String args){
+        System.out.println("Output buổi 1");
+        System.out.println("Done cài đặt JDK");
+        System.out.println("Done cài đặt IJ");
+        System.out.println("Tạo được project git và gửi link cho anh");
+    }
+}
