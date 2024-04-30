ee38c275d6650acc80f04dbcaa73f629d9225537
NQuynhAnh <111996607+NQuynhAnh@users.noreply.github.com>
2024-04-30 07:22:31 +0700

    Update Main.java

--- Demo/src/Main.java
+++ Demo/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Xin chào lớp Java Bacsic 01-2024");
         System.out.println("Rất vui được gặp bạn");
+        System.out.println("Test trường hợp pull code về máy");
     }
 }
